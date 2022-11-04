cc32b61d9de18472caa261b16dd39644c57b0c7b
Tyler Grant <Tylerbb812@yahoo.com>
2022-11-04 10:42:59 -0600

    added buttons to main page for creating things they do nothing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoomModalComponent } from './room-modal/room-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomModalComponent
   ],
   imports: [
     BrowserModule,
